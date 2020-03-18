b847c7a0753cc62e93ff03c1de6a43293bfd502f
mykola829 <40271583+mykola829@users.noreply.github.com>
2020-03-18 12:58:14 +0200

    edited labs 1-3 sql scripts

--- lab1.sql
+++ lab1.sql
@@ -4,11 +4,14 @@ create database if not exists projectManagementSystem;
 
 use projectManagementSystem;
 
+
 create table if not exists project(
 	project_id int unsigned not null auto_increment,
     project_name varchar(40) not null,
+    customer_company varchar(40) not null,
     primary key(project_id)
 );	
+
 
 create table if not exists contacts(
 	contacts_id int unsigned not null auto_increment,
@@ -20,30 +23,18 @@ create table if not exists contacts(
 
 create table if not exists user(
 	user_id int unsigned not null auto_increment,
-    contacts_id int unsigned not null,
+    contacts_id int unsigned unique not null ,
     surname varchar(30) not null,
     name varchar(30) not null,
     biography text,
     login varchar(40) not null,
     password varchar(40) not null,
-	constraint user_contacts foreign key (contacts_id) references contacts (contacts_id) on delete no action on update no action,
+	constraint user_contacts foreign key (contacts_id) references contacts (contacts_id) on delete cascade on update cascade,
     primary key(user_id)
-);
-
-
-create table if not exists user_project(
-	user_project_id int unsigned not null auto_increment,
-    user_id int unsigned not null,
-    project_id int unsigned not null,
-    user_role enum('admin', 'user'),
-    primary key(user_project_id),
-    foreign key (user_id) references user (user_id) on delete cascade on update cascade,
-    foreign key (project_id) references project (project_id) on delete cascade on update cascade
 );
 
 create table if not exists task(
 	task_id int unsigned not null auto_increment,
-    user_id int unsigned not null,
     deadline_time datetime not null,
     completion_time datetime,
     title varchar(50) not null,
@@ -52,25 +43,32 @@ create table if not exists task(
     primary key (task_id)
 );
 
+create table if not exists user_project_task(
+	user_project_task_id int unsigned not null auto_increment,
+    user_id int unsigned not null,
+    task_id int unsigned not null,
+    project_id int unsigned not null,
+    user_role enum('admin', 'user') default 'user',
+    primary key(user_project_task_id),
+    foreign key (user_id) references user (user_id) on delete cascade on update cascade,
+    foreign key (project_id) references project (project_id) on delete cascade on update cascade,
+    foreign key (task_id) references task (task_id) on delete cascade on update cascade
+);
+
+
+
 create table if not exists task_comment(
 	comment_id int unsigned not null auto_increment,
 	user_id int unsigned not null,
 	task_id int unsigned not null,
-    text text not null,
+    comment_text text not null,
     posted_time datetime not null,
     primary key(comment_id),
     constraint user_comment foreign key (user_id) references user (user_id) on delete no action on update no action,
     constraint task_comment foreign key (task_id) references task (task_id) on delete cascade on update cascade
 );
 
-create table if not exists task_activity(
-	task_activity_id int unsigned not null auto_increment,
-	task_id int unsigned not null,
-    user_id int unsigned not null,
-    activity_description text not null,
-    activity_time datetime not null,
-	primary key (task_activity_id),
-	constraint task_task_activity foreign key (task_id) references task (task_id) on delete cascade on update cascade,
-    constraint user_task_activity foreign key (user_id) references user (user_id) on delete cascade on update cascade
-);
+show tables;
+
+
 
--- lab3.sql
+++ lab3.sql
@@ -1,8 +1,8 @@
 use projectManagementSystem;
 
-
 alter table task_comment
 	drop foreign key user_comment,
+    drop index user_comment,
     drop column posted_time;
     
 show create table task_comment;
@@ -19,6 +19,17 @@ alter table user
 show create table user;
 
 alter table task_comment
-	add constraint user_comment foreign key (user_id) references user (user_id) on delete no action on update no action;
+	add constraint user_comment foreign key (user_id) references user (user_id);
     
+show create table task_comment;
+
+alter table user
+	drop foreign key user_contacts;
+    
+show create table user;
+
+alter table user
+	add constraint user_contacts FOREIGN KEY (contacts_id)
+	references contacts (contacts_id) ON DELETE cascade on update cascade;
+
 show create table task_comment;
